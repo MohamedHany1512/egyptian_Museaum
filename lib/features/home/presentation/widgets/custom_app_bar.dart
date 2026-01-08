106694a1299d0e6561b68110556a17decdb5e5c4
MohamedHA=any1512 <mh861590@gmail.com>
2026-01-08 21:44:12 +0200

    done app bar

---
+++ lib/features/home/presentation/widgets/custom_app_bar.dart
@@ -0,0 +1,34 @@
+
+import 'package:egyptain_museaum/core/widgets/custom_bold_text.dart';
+import 'package:egyptain_museaum/features/home/presentation/widgets/custom_search.dart';
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(Icons.language_sharp, size: 24),
+        const SizedBox(width: 4),
+        CustomBoldText(text: "AR", fontSize: 14),
+        const SizedBox(width: 6),
+
+        Expanded(
+          child: CustomSearch(
+            labelText: "Floor1",
+
+            textInputAction: TextInputAction.done,
+          ),
+        ),
+        const SizedBox(width: 20),
+
+        CircleAvatar(
+          radius: 18,
+          child: Image.asset('assets/images/ai_Guide.png'),
+        ),
+      ],
+    );
+  }
+}
